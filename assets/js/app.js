f4f62b0124e5aa0712720fcc572739f112dd634c
AdrengT <adriant@psbhq.com>
2019-09-30 13:10:46 -0700

    Cancelling Project until further notice. Will probably continue it during free time

--- assets/js/app.js
+++ assets/js/app.js
@@ -17,3 +17,28 @@ $('#generate').on('click', function(e) {
 	$('.sku-container').text(sku);
 	//$('.quantity-container').text('qr')
 });
+
+$('#download-pdf button').on('click', function(e) {
+	var element = document.getElementById('pdf-gen');
+	var opt = {
+		margin:       1,
+		filename:     'myfile.pdf',
+		image:        { type: 'png' },
+		html2canvas:  { scale: 2 },
+		jsPDF:        { unit: 'in', format: 'letter', orientation: 'portrait' }
+	};
+
+	html2pdf().set(opt).from(element, 'element').save();
+});
+
+/* 
+var pdf = new jsPDF('p', 'pt', 'letter');
+	pdf.html(document.getElementById('pdf-gen'), {
+		callback: function (pdf) {
+			var iframe = document.createElement('iframe');
+			iframe.setAttribute('style', 'position:absolute;right:0; top:0; bottom:0; height:100%; width:500px');
+			document.body.appendChild(iframe);
+			iframe.src = pdf.output('datauristring');
+		}
+	}
+); */
