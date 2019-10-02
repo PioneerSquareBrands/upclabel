ce608c841b06328c467483e61572f23c4c540edc
AdrengT <adriant@psbhq.com>
2019-10-02 14:44:33 -0700

    Added Brenthaven option and a lot more

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,26 +1,67 @@
 $(document).foundation()
 
 $(window).on('load', function() {
+	switchDefault();
 	$('#generator_form').trigger('change');
 });
 
-$("#generator_form input[type='text']").on("click", function () {
-	$(this).select();
-});
-
 function generate() {
-	var itemMaster = $('#item_master').val();
-	var upc = $('#upc').val();
-	var sku = $('#sku').val();
-	var desc = $('#description').val();
-	var qty = $('input[name="qty"]:checked').val();
-	var qr = $('#qr').val();
+	var brand = $('#brand').val();
+	var upcHeading = $('#upc_header').val();
+	$('.upc-label--heading').text(upcHeading);
 
-	JsBarcode("#upc-svg", upc, {format: "upc"});
-	$('.master-container').text(itemMaster);
-	$('.description-container').text(desc);
-	$('.sku-container').text(sku);
-	$('.quantity-container .qty-val').text(qty)
+	if (brand == 'GD') {
+		$('#gd_label').show();
+		$('#bh_label').hide();
+
+		var itemMaster = $('#item_master').val();
+		var upc = $('#upc').val();
+		var sku = $('#sku').val();
+		var desc = $('#description').val();
+		var qtyLabel = $('#quantity_label').val();
+		var qty = $('input[name="qty"]:checked').val();
+		var qr = $('#qr').val();
+
+		JsBarcode("#gd_label #upc-svg", upc, {format: "upc"});
+		$('#gd_label .master-container').text(itemMaster);
+		$('#gd_label .description-container').text(desc);
+		$('#gd_label .sku-container').text(sku);
+		$('#gd_label .quantity-container .qty-label').text(qtyLabel + ':')
+		$('#gd_label .quantity-container .qty-val').text(qty)
+		$('#gd_label #qrcode').html('');
+		$('#gd_label #qrcode').qrcode({width: 75, height: 75, text: qr});
+		$('#gd_label .qr-link').text(qr);
+	} 
+	else if(brand == 'BH') {
+		$('#bh_label').show();
+		$('#gd_label').hide();
+
+		var itemMaster = $('#item_master').val();
+		var upc = $('#upc').val();
+		var sku = $('#sku').val();
+		var desc = $('#description').val();
+		var qtyLabel = $('#quantity_label').val();
+		var qty = $('input[name="qty"]:checked').val();
+		var qr = $('#qr').val();
+
+		JsBarcode("#bh_label #upc-svg", upc, {format: "upc"});
+		$('#bh_label .master-container').text(itemMaster);
+		$('#bh_label .description-container').text(desc);
+		$('#bh_label .sku-container').text(sku);
+		$('#bh_label .quantity-container .qty-label').text(qtyLabel + ':')
+		$('#bh_label .quantity-container .qty-val').text(qty)
+		$('#bh_label #qrcode').html('');
+		$('#bh_label #qrcode').qrcode({width: 75, height: 75, text: qr});
+		$('#bh_label .qr-link').text(qr);
+	}
+}
+
+function switchDefault() {
+	$('.switch-default').each(function() {
+		var brand = $('#brand').val().toLowerCase();
+		var defVal = $(this).data(brand);
+		$(this).val(defVal);
+	});
 }
 
 $('#generator_form').on('change keyup paste', function(e) {
@@ -32,19 +73,18 @@ $('#generate').on('click', function(e) {
 	generate();
 });
 
+$('#brand').on('change', function() {
+	switchDefault();
+})
+
 $('#download-pdf').on('click', function(e) {
 	e.preventDefault();
-	var fileName =  $('#item_master').val() + ' (' + $('input[name="qty"]:checked').val() + 'pack).pdf';
+	var brand = $('#brand').val();
+	var itemMaster = $('#item_master').val();
+	var qtyLabel = $('#quantity_label').val().replace(' ', '_');
+	var qty = $('input[name="qty"]:checked').val();
 
-	/* var pdf = new jsPDF('p', 'pt', 'letter');
-	pdf.html(document.getElementById('generated_wrapper'), {
-		callback: function (pdf) {
-			var iframe = document.createElement('iframe');
-			iframe.setAttribute('style', 'position:absolute;right:0; top:0; bottom:0; height:100%; width:500px');
-			document.body.appendChild(iframe);
-			iframe.src = pdf.output('datauristring');
-		}
-	}); */
+	var fileName =  brand + '-' + itemMaster + ' (' + qtyLabel + '-' + qty + ').pdf';
 	var element = document.getElementById('generated_wrapper');
 	var opt = {
 		margin: 1,
